44225119c57646379236e530fbe3fc2eecf7b253
Shubham Kumar <shubhamkm5382@gmail.com>
2025-07-17 04:17:24 +0530

    Arrow functions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+//if 
+const isUserLoggedIn = true
+const temperature = 50
+
+if (temperature < 50 ) {
+    console.log("temperature is less than 50");
+} else {
+    console.log("temperature is greater than or equal to 50");
+}
+
+console.log("executed");
+
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
+
+// console.log(`user power: ${power}`);
+
+const balance = 1000;
+
+if (balance < 500) console.log("test"), console.log("test2");
+console.log("test3");
+
+
+
